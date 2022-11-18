a3ea6de30649eccbae7cdbbd0773ff743bfa2638
marzad <maschatz@gmx.net>
2022-11-18 11:22:35 +0100

    Add files to git-repository

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import ToDoApp from "./ToDoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoApp/>
+    </div>
+  );
+}
+
+export default App;
